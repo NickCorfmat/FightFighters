fe275256f062071d369ff74a445fa0404ad70887
“Nicolas <ncorfmat@ucsc.edu>
2024-02-09 19:09:57 -0800

    Working menu version 1

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,21 +3,28 @@ class Menu extends Phaser.Scene {
         super("menuScene")
     }
 
-    init() {
-
-    }
-
     preload() {
-        // load images/tile sprites
-        this.load.image('background', './assets/road_background.png')
+        // load images/spritesheets/tile sprites
+        this.load.image('menu_background', './assets/menu_background.png')
         this.load.image('cone', './assets/cone.png')
-        this.load.image('player', './assets/player.png')
+        this.load.image('edge', './assets/line.png')
+        this.load.image('side-plane', './assets/plane_sideview.png')
 
-        // load audio
+        this.load.spritesheet('title', './assets/title.png', {
+            frameWidth: 110,
+            frameHeight: 40
+        })
 
+        this.load.spritesheet('player', './assets/plane.png', {
+            frameWidth: 80,
+            frameHeight: 120
+        })
+        
     }
 
     create() {
+
+        // text config
         let menuConfig = {
             fontFamily: 'Courier',
             fontSize: '28px',
@@ -35,26 +42,47 @@ class Menu extends Phaser.Scene {
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
 
         // menu background
-        //this.add.image(0, 0, 'menuScreen').setOrigin(0)
+        this.background = this.add.tileSprite(0, 0, 300, 200, 'menu_background').setOrigin(0).setScale(4)
 
         // add text
         this.add.text(width/2, height/2, 'Press SPACE to start', menuConfig).setOrigin(0.5)
+        
+        // title animation configuration
+        this.anims.create({
+            key: 'gameTitle',
+            frameRate: 2,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('title', {
+                start: 0,
+                end: 1
+            })
+        })
 
-        this.cameras.main.setBackgroundColor('#AAAAAA')
+        // plane sprite
+        this.plane = this.physics.add.sprite(width/4, 2*height/3, 'side-plane').setOrigin(0).setScale(3)
+
+        // title sprite
+        this.title = this.add.sprite(width/2, height/4).setScale(6)
+        this.title.play('gameTitle')
     }
 
     update() {
+        // scroll background
+        this.background.tilePositionX += 1
+
+        // automatic plane movement
+        const accelerationX = this.plane.x < width/2 ? 20 : -50
+        const accelerationY = this.plane.y >= 3*height/5 ? -10 : 15
+
+        this.plane.body.setAccelerationY(accelerationY)
+        this.plane.body.setAccelerationX(accelerationX)
 
         // select Play scene
         if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
-            // this.cameras.main.fadeOut(250, 255, 255, 255)
-            // this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
-            //     this.scene.start('playScene')
-            // })
-
-            this.scene.start('playScene')
+            this.cameras.main.fadeOut(2000, 255, 255, 255)
+            this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+                this.scene.start('playScene')
+            })
         }
-
-        // select Instructions scene
     }
 }
