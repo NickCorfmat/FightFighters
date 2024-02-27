76d244bb576e5afbbf1fe3e90740b08f7c7d4481
“Nicolas <ncorfmat@ucsc.edu>
2024-02-27 12:02:04 -0800

    fade-in/out scene transitions

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,15 +5,26 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-        this.cameras.main.setBackgroundColor('#FACADE')
-        
-        keyENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
+        // setup keyboard input
+        this.keys = this.input.keyboard.createCursorKeys()
+        this.keys.Enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
+
+        //this.background = this.add.sprite(width/2, height/2, 'menu_background').setOrigin(0.5).setScale(0.55)
+        this.cameras.main.setBackgroundColor('0x000000')
+        this.add.rectangle(width/2, height/3, width, 200, 0xad79db).setOrigin(0.5)
+        this.add.sprite(width/2, height/3, 'title').setOrigin(0.44, 0.5).setScale(9)
+
+       // this.add.line(0, 0, 600, 0, 600, 1600, 0xffffff)
+
     }
 
     update() {
         // check for transition to play scene
-        if (Phaser.Input.Keyboard.JustDown(keyENTER)) {
-            this.scene.start('playScene')
+        if (Phaser.Input.Keyboard.JustDown(this.keys.Enter)) {
+            this.cameras.main.fadeOut(250, 255, 255, 255)
+            this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, (cam, effect) => {
+                this.scene.start('selectScene')
+            })
         }
 
     }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,19 +8,20 @@ class Play extends Phaser.Scene {
     }
 
     create() {
+        this.cameras.main.fadeIn(50, 255, 255, 255)
+
         // add animated background
         this.background = this.add.sprite(0, 0, 'background').setOrigin(0).setScale(2)
         this.background.play('fight-background')
 
+        // set custom world bounds
         this.physics.world.setBounds(-50, 0, width + 100, 7*height/8)
 
         // add new fighters to scene (scene, x, y, key, frame, direction, health, speed)
-        // this.player = this.physics.add.sprite(0,0,'sticky').setOrigin(0)
-        // this.player.body.setGravityY(2500)
-        // this.player.body.setCollideWorldBounds(true)
         this.player1 = new Rumble(this, 375, 600, 'sticky', 0, 'right', 100, 500)
-        this.player2 = new DrKarate(this, 825, 700, 'dr-karate-play', 0, 'left', 100, 500)
+        this.player2 = new DrKarate(this, 825, 600, 'dr-karate-play', 0, 'left', 100, 500)
 
+        // add name cards under health bar
         this.add.sprite(148, 165, 'rumble-play-text').setOrigin(0).setScale(1.75)
         this.add.sprite(790, 165, 'karate-play-text').setOrigin(0).setScale(1.75)
 
@@ -40,6 +41,10 @@ class Play extends Phaser.Scene {
         this.keys.Enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
         this.keys.keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
 
+        this.keys.key1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE)
+        this.keys.key2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO)
+
+        // beginning 'FIGHT' sequence
         this.fight = this.add.sprite(width/2, height/2, 'FIGHT').setOrigin(0.5).setScale(5)
 
         this.time.delayedCall(1500, () => {
@@ -47,21 +52,22 @@ class Play extends Phaser.Scene {
             this.fight.destroy()
         })
         
-        //this.add.line(0, 0, 600, 0, 600, 1600, 0x000000);
+        //this.add.line(0, 0, 600, 0, 600, 1600, 0x000000)
     }
 
     update() {
+        // temporary scene restart function
         if (Phaser.Input.Keyboard.JustDown(this.keys.keyR)) {
             this.scene.restart()
         }
 
         // check if game over
-        if (this.player1.HP <= 0) {
+        if (this.player1.HP <= 0 || Phaser.Input.Keyboard.JustDown(this.keys.key1)) {
             this.add.sprite(600, 400, 'KO').setOrigin(0.45, 0.5).setScale(12)
             this.time.delayedCall(1800, () => {
                 this.scene.start('rumbleWinnerScene')
             })
-        } else if (this.player2.HP <= 0) {
+        } else if (this.player2.HP <= 0 || Phaser.Input.Keyboard.JustDown(this.keys.key2)) {
             this.add.sprite(600, 400, 'KO').setOrigin(0.45, 0.5).setScale(12)
             this.time.delayedCall(1750, () => {
                 this.scene.start('karateWinnerScene')
