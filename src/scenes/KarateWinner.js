de8971a25e1bdc26c0f1707c69c29024044449d1
“Nicolas <ncorfmat@ucsc.edu>
2024-02-27 12:01:18 -0800

    added/modified anims

--- src/scenes/KarateWinner.js
+++ src/scenes/KarateWinner.js
@@ -4,26 +4,29 @@ class KarateWinner extends Phaser.Scene {
     }
 
     create() {
-        this.background = this.add.tileSprite(0, 0, 158, 106, 'fire-background').setOrigin(0).setScale(8)
+        // background
+        this.background = this.add.tileSprite(0, 0, 158, 106, 'pink-fire-background').setOrigin(0).setScale(8)
 
-        this.karate = this.add.sprite(600, 450, 'Dr_Karate').setOrigin(0.5).setScale(1.25)
-        this.karate.anims.play('karate-spin').once('animationcomplete', () => {
-            this.karate.y = 410
-            this.karate.anims.play('karate-speech').once('animationcomplete', () => {
-                this.karate.anims.play('karate-speech-end')
+        // winner name text
+        this.karateName = this.add.sprite(width/2, 225, 'karate-name').setOrigin(0.5).setScale(3)
 
-                this.input.keyboard.on('keydown-M', function() {
-                    this.scene.start('menuScene')
-                }, this)
-            })
+        // winner sprite animation
+        this.karate = this.add.sprite(600, 500, 'Dr_Karate').setOrigin(0.5).setScale(4)
+
+        this.karate.anims.play('karate-pose').once('animationcomplete', () => {
+            this.karate.anims.play('karate-pose-end')
+
+            this.input.keyboard.on('keydown-M', function() {
+                this.scene.start('menuScene')
+            }, this)
         })
 
+        // black borders
         this.add.rectangle(0, 0, width, 174, 0x000000).setOrigin(0)
         this.add.rectangle(0, 634, width, 170, 0x000000).setOrigin(0)
     }
 
     update() {
-        this.background.tilePositionX += 7
-
+        this.background.tilePositionX += 8
     }
 }
--- src/scenes/RumbleWinner.js
+++ src/scenes/RumbleWinner.js
@@ -4,13 +4,18 @@ class RumbleWinner extends Phaser.Scene {
     }
 
     create() {
+        // background
         this.background = this.add.tileSprite(0, 0, 184, 102, 'fire-background').setOrigin(0).setScale(8)
+
+        // winner name text
         this.rumbleName = this.add.sprite(width/2, 225, 'rumble-name').setOrigin(0.5).setScale(3)
 
-        this.speech = this.sound.add('rumble-winner', { loop: false })
+        // play audio
+        this.speech = this.sound.add('rumble-winner')
         this.speech.play()
 
-        this.rumble = this.add.sprite(600, 450, 'Rumble_McSkirmish').setOrigin(0.5).setScale(1.25)
+        // winner sprite animation
+        this.rumble = this.add.sprite(600, 450, 'Rumble_McSkirmish').setOrigin(0.5).setScale(7.5)
         this.rumble.anims.play('rumble-spin').once('animationcomplete', () => {
             this.rumble.y = 410
             this.rumbleName.destroy()
@@ -23,6 +28,7 @@ class RumbleWinner extends Phaser.Scene {
             })
         })
 
+        // black borders
         this.add.rectangle(0, 0, width, 174, 0x000000).setOrigin(0)
         this.add.rectangle(0, 634, width, 170, 0x000000).setOrigin(0)
 
@@ -30,6 +36,5 @@ class RumbleWinner extends Phaser.Scene {
 
     update() {
         this.background.tilePositionX += 8
-
     }
 }
